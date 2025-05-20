b6367f650ada93ba4b08837e5f804515100d4e50
luanfelixlima <luan.felix.lima@outlook.com>
2025-05-20 10:25:23 -0300

    Implementando tela de acesso negado

---
+++ src/main/java/com/fesa/sharetools/Controller/ErrorController.java
@@ -0,0 +1,14 @@
+package com.fesa.sharetools.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "access-denied"; // Retorna access-denied.html
+    }
+}
+
